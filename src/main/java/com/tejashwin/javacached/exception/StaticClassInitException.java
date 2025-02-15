3bf59f12cc7f8485858e292d403bc3a6a10e67a1
tejashwin.u <tejashwin5104@gmail.com>
2025-02-15 11:35:12 +0530

    Moved run to separate static class
    Signed-off-by: tejashwin.u <tejashwin5104@gmail.com>

---
+++ src/main/java/com/tejashwin/javacached/exception/StaticClassInitException.java
@@ -0,0 +1,10 @@
+package com.tejashwin.javacached.exception;
+
+
+public class StaticClassInitException extends RuntimeException {
+
+
+    public StaticClassInitException(String clazzName) {
+        throw new IllegalStateException("Cannot create object for " + clazzName);
+    }
+}
