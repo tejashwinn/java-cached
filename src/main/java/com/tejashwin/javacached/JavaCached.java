398f4c530d6ff6a06a38c776e3ce46407f706cba
tejashwinn. <tejashwin.uday@gmail.com>
2025-02-15 11:40:38 +0530

    Moved run to separate static class (#1)
    Signed-off-by: tejashwin.u <tejashwin5104@gmail.com>

--- src/main/java/com/tejashwin/javacached/JavaCached.java
+++ src/main/java/com/tejashwin/javacached/JavaCached.java
@@ -1,28 +1,19 @@
 package com.tejashwin.javacached;
 
+import com.tejashwin.javacached.exception.StaticClassInitException;
+import com.tejashwin.javacached.run.JavaCachedApplication;
 import lombok.extern.slf4j.Slf4j;
-
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 @Slf4j
 public class JavaCached {
 
-    public static void main(String[] args) {
-        log.info("Started java cached");
-        int port = 6379;
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-            // Since the tester restarts your program quite often, setting SO_REUSEADDR
-            // ensures that we don't run into 'Address already in use' errors
-            serverSocket.setReuseAddress(true);
-            // Wait for connection from client.
-            Socket clientSocket = serverSocket.accept();
-
-        } catch (IOException e) {
-            log.error(e.getMessage(), e);
-        } finally {
-            log.info("Stopped java cached");
-        }
+    private JavaCached() {
+        throw new StaticClassInitException(JavaCached.class.getName());
     }
+
+    public static void main(String[] args) {
+        JavaCachedApplication.run(args);
+    }
+
+
 }
